142f8fdf4ae655f79f5610fd3af6cfc6f05dd8ce
Parcer Luka <Luka.Parcer@rtvslo.si>
2021-10-29 15:45:22 +0200

    Finaly all data working and ready to publish

--- getweatherstat.js
+++ getweatherstat.js
@@ -26,13 +26,20 @@ function(err, data) {
 			return
 		}
 
-		//console.log("Data reads: " + JSON.stringify(data))
+		// outdoor data
 		document.getElementById("outsideTemp").textContent = Number(data[0].modules[0].temp).toFixed(1)
 		document.getElementById("minOutsideTemp").textContent = data[0].modules[0].min_temp
 		document.getElementById("maxOutsideTemp").textContent = data[0].modules[0].max_temp
 		document.getElementById("pressure").textContent = data[0].pressure
 		document.getElementById("outsideHumidity").textContent = data[0].modules[0].humidity
 		document.getElementById("rain").textContent = data[0].modules[2].rainLastMeasured
+		// indoor data 1
+		document.getElementById("indoorTemp1").textContent = Number(data[0].temp).toFixed(1)
+		document.getElementById("minIndoorTemp1").textContent = data[0].min_temp
+		document.getElementById("maxIndoorTemp1").textContent = data[0].max_temp
+		document.getElementById("indoorCO21").textContent = data[0].CO2
+		document.getElementById("indoorNoise").textContent = data[0].noise
+		document.getElementById("indoorHumidity1").textContent = data[0].humidity
 
 		// trend for outdoor temp
 		let outTrend = data[0].modules[0].trend_temp
@@ -41,6 +48,15 @@ function(err, data) {
 			outTrendCaret.className = "bi bi-caret-up-fill"
 		}else if (outTrend == "down") {
 			outTrendCaret.className = "bi bi-caret-down-fill"
+		}
+
+		// trend for indoor temp 1
+		let indoorTempTrend1 = data[0].trend_temp
+		const indoorTempTrend1Caret = document.getElementById("indoorTempTrend1")
+		if (indoorTempTrend1 == "up") {
+			indoorTempTrend1Caret.className = "bi bi-caret-up-fill"
+		}else if (indoorTempTrend1 == "down") {
+			indoorTempTrend1Caret.className = "bi bi-caret-down-fill"
 		}
 
 		//trend for pressure
@@ -52,7 +68,7 @@ function(err, data) {
 			pressureTrendCaret.className = "bi bi-caret-down-fill"
 		}
 
-		const theSvg = document.getElementById("svg")
-		theSvg.setAttribute("transform", "rotate(180)");
+		// const theSvg = document.getElementById("svg")
+		// theSvg.setAttribute("transform", "rotate(180)");
   }
 });
